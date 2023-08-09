21f6bbc226562855a238b1302539740fe21d9730
Eng Muhammad Riaz <ranariaz8686@gmail.com>
2023-08-09 11:15:44 +0500

    login activity

--- app/src/main/java/com/example/pny/SplashActivity.java
+++ app/src/main/java/com/example/pny/SplashActivity.java
@@ -3,22 +3,33 @@ package com.example.pny;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        sharedPreferences = getSharedPreferences("pny", MODE_PRIVATE);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                if(sharedPreferences.getBoolean("isLogin", false)) {
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
-        },5000);
+        }, 5000);
     }
 }
